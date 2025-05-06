b999efb51522a8c42261982781bd941ae3a89ce0
Muhammad Uzair <uzairshaikh346@gmail.com>
2025-05-06 20:23:26 +0500

    set load

--- src/app/components/contactForm.tsx
+++ src/app/components/contactForm.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useState } from "react";
 import {motion} from "framer-motion"
+import Image from "next/image";
 function ContactForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -30,17 +31,17 @@ function ContactForm() {
 
         </p>
         <div className="flex items-center">
-          <img width="35" height="35" src="https://img.icons8.com/windows/32/b8a9fe/phone.png" alt="phone--v1"/>
+          <Image width="35" height="35" src="https://img.icons8.com/windows/32/b8a9fe/phone.png" alt="phone--v1"/>
           <p className="font-bold md:text-4xl ml-5">Phone: (+971) 528 977 529</p>
         </div>
 
         <div className="flex items-center mt-5">
-          <img width="35" height="35" src="https://img.icons8.com/ios/50/b8a9fe/new-post--v1.png" alt="mail"/>
+          <Image width="35" height="35" src="https://img.icons8.com/ios/50/b8a9fe/new-post--v1.png" alt="mail"/>
           <p className="font-bold md:text-4xl ml-5">Email: info@Orionsols.com</p>
         </div>
 
         <div className="flex items-center mt-5">
-          <img width="35" height="35" src="https://img.icons8.com/ios/50/b8a9fe/marker--v1.png" alt="marker--v1"/>
+          <Image width="35" height="35" src="https://img.icons8.com/ios/50/b8a9fe/marker--v1.png" alt="marker--v1"/>
           <p className="font-bold md:text-4xl ml-5">Address: Ascote Park Place office 401,Dubai</p>
         </div>
       </div>
