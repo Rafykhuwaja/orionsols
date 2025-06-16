32e902cb59d7ba4ac30dfbb31ba94cdffb86f6bb
Muhammad Uzair <uzairshaikh346@gmail.com>
2025-06-16 16:48:09 +0500

    set error of google analytics

--- src/app/lib/gtag.ts
+++ src/app/lib/gtag.ts
@@ -1,6 +1,17 @@
 // lib/gtag.ts
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || '';
 
+// Declare gtag as a property on the Window interface
+declare global {
+  interface Window {
+    gtag: (
+      command: string,
+      targetId: string,
+      config?: Record<string, any>
+    ) => void;
+  }
+}
+
 // Pageview tracking
 export const pageview = (url: string) => {
   if (typeof window !== 'undefined' && GA_TRACKING_ID) {
